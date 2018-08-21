d6bb104cc9620fcc97d517494a0bb9f5a4241f61
Jônatas Trabuco Belotti <jonatas.t.belotti@hotmail.com>
2018-08-21 15:35:10 -0300

    Forçando utilização do SSL

--- javascript/redirecionamento.js
+++ javascript/redirecionamento.js
@@ -1,11 +1,15 @@
-var SITE_OFICIAL = "jonatastbelotti.com";
+var SITE_OFICIAL = "https://www.jonatastbelotti.com";
 
 redirecionarSiteOficial();
 
 function redirecionarSiteOficial() {
     var url = location.href;
 
+    if (url.startsWith("file")) {
+        return;
+    }
+
     if (url.indexOf(SITE_OFICIAL) < 0) {
-        window.location.replace("http://www." + SITE_OFICIAL);
+        window.location.replace(SITE_OFICIAL);
     }
 }
